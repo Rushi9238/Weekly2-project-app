da0f1c95c5ac2c80d37a2dd01c40aed9b2cc1f8e
Rushikesh Patil <patilrushikesh8683@gmail.com>
2023-02-27 15:59:37 +0530

    Weekly test 2 project app using React

---
+++ src/Components/FooterContain.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const FooterContain = (props) => {
+  return (
+   <>
+   <div className="footer-css">
+       <h2>{props.h2}</h2>
+       <p>{props.p}</p>
+       <p>{props.p1}</p>
+       <p>{props.p2}</p>
+       <p>{props.p3}</p>
+       <img src={props.img}></img>
+ </div>
+   </>
+  )
+}
+
+export default FooterContain
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../CSS_files/Navbar.css'
+const Navbar = () => {
+  return (
+    <>
+    <div className='navbardiv'>
+
+        <h1><span>P</span>roject</h1>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Project</li>
+            <li>Blog</li>
+        </ul>
+        <button>click</button>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
